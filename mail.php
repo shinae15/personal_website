831effe8bdda38b917b8e21f0cae719a1ddb530d
shinae15 <78455955+shinae15@users.noreply.github.com>
2021-09-27 20:22:24 -0700

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,19 @@
+<?php
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+
+$to = "belovedrong@gmail.com";
+
+$subject = "Mail From My Website";
+
+$txt ="Name = ". $name . "\r\nEmail = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@yoursite.com" . "\r\n" .
+"CC: lees145@unlv.nevada.edu";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
